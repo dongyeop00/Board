1a99be67e1d4754a7e7eb4f7872d951d3c8a1c0e
dongyeop00 <ehd_duq94@naver.com>
2024-01-19 01:58:58 +0900

    feat : Paging 처리

--- src/main/java/com/gdy/board/Controller/BoardController.java
+++ src/main/java/com/gdy/board/Controller/BoardController.java
@@ -3,6 +3,9 @@ package com.gdy.board.Controller;
 import com.gdy.board.DTO.BoardDTO;
 import com.gdy.board.Service.BoardService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -37,13 +40,14 @@ public class BoardController {
     }
 
     @GetMapping("/{id}")
-    public String findById(@PathVariable Long id, Model model){
+    public String findById(@PathVariable Long id, Model model, @PageableDefault(page=1) Pageable pageable){
         // 1. 해당 게시글의 조회수를 하나 올린다.
         boardService.updateHits(id);
 
         // 2. 게시글 데이터를 가져와서 detail.html에 출력한다.
         BoardDTO boardDTO = boardService.findById(id);
         model.addAttribute("board", boardDTO);
+        model.addAttribute("page",pageable.getPageNumber());
         return "detail";
     }
 
@@ -66,5 +70,21 @@ public class BoardController {
     public String delete(@PathVariable Long id){
         boardService.delete(id);
         return "redirect:/board/"; //리스트로 이동
+    }
+
+    @GetMapping("/paging") // /board/paging?page=1
+    public String paging(@PageableDefault(page=1)Pageable pageable, Model model){
+        // 한페이지에 몇개씩 보냐에 따라 전체 페이지 수가 달라짐
+        //int pageNumber = pageable.getPageNumber();
+        Page<BoardDTO> boardList = boardService.paging(pageable);
+        int blockLimit = 3;
+        int startPage = (((int)(Math.ceil((double)pageable.getPageNumber() / blockLimit))) - 1) * blockLimit + 1; // 1 4 7 10 ~~
+        int endPage = ((startPage + blockLimit - 1) < boardList.getTotalPages()) ? startPage + blockLimit - 1 : boardList.getTotalPages();
+        // 7 8 9 보여줘야 하는데 전체 페이지가 8이면 8을 보여줘라
+
+        model.addAttribute("boardList",boardList);
+        model.addAttribute("startPage",startPage);
+        model.addAttribute("endPage",endPage);
+        return "paging";
     }
 }
